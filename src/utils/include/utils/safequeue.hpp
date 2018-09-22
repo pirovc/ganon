928953bda20cce53744b3aa4b1cc414f575e5861
Diogo Andrei Benvenutti <diogo.a.benvenutti@gmail.com>
2018-09-22 18:02:52 +0200

    Run format on SafeQueue

--- src/utils/include/utils/safequeue.hpp
+++ src/utils/include/utils/safequeue.hpp
@@ -1,27 +1,36 @@
-template <class T>
-class SafeQueue{
-    private:
-        std::queue<T> q;
-        std::mutex m;
-    public:
-        void push(T t){
-            std::lock_guard<std::mutex> lock(m);
-            q.push(t);
-        }
-        T pop(){
-            std::lock_guard<std::mutex> lock(m);
-            if ( q.empty() )
-                return T();
-            T val = q.front();
-            q.pop();
-            return val;
-        }
-        int size(){
-            std::lock_guard<std::mutex> lock(m);
-            return q.size();
-        }
-        bool empty(){
-            std::lock_guard<std::mutex> lock(m);
-            return q.empty();
-        }
+template < class T >
+class SafeQueue
+{
+private:
+    std::queue< T > q;
+    std::mutex      m;
+
+public:
+    void push( T t )
+    {
+        std::lock_guard< std::mutex > lock( m );
+        q.push( t );
+    }
+
+    T pop()
+    {
+        std::lock_guard< std::mutex > lock( m );
+        if ( q.empty() )
+            return T();
+        T val = q.front();
+        q.pop();
+        return val;
+    }
+
+    int size()
+    {
+        std::lock_guard< std::mutex > lock( m );
+        return q.size();
+    }
+
+    bool empty()
+    {
+        std::lock_guard< std::mutex > lock( m );
+        return q.empty();
+    }
 };
