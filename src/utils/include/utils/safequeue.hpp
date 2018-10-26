c91f20ea48789555cbc917978e96d2910ac5baf7
benvenutti <diogo.a.benvenutti@gmail.com>
2018-10-26 23:22:47 +0200

    Rename member variables

--- src/utils/include/utils/safequeue.hpp
+++ src/utils/include/utils/safequeue.hpp
@@ -6,36 +6,36 @@
 template < class T >
 class SafeQueue
 {
-private:
-    std::queue< T > q;
-    std::mutex      m;
-
 public:
     void push( T t )
     {
-        std::lock_guard< std::mutex > lock( m );
-        q.push( t );
+        std::lock_guard< std::mutex > lock( m_mutex );
+        m_queue.push( t );
     }
 
     T pop()
     {
-        std::lock_guard< std::mutex > lock( m );
-        if ( q.empty() )
+        std::lock_guard< std::mutex > lock( m_mutex );
+        if ( m_queue.empty() )
             return T();
-        T val = q.front();
-        q.pop();
+        T val = m_queue.front();
+        m_queue.pop();
         return val;
     }
 
     int size()
     {
-        std::lock_guard< std::mutex > lock( m );
-        return q.size();
+        std::lock_guard< std::mutex > lock( m_mutex );
+        return m_queue.size();
     }
 
     bool empty()
     {
-        std::lock_guard< std::mutex > lock( m );
-        return q.empty();
+        std::lock_guard< std::mutex > lock( m_mutex );
+        return m_queue.empty();
     }
+
+private:
+    std::queue< T > m_queue;
+    std::mutex      m_mutex;
 };
