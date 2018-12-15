8fa8ed5e7621d9356cf8e69e06c8623675fb42a6
Vitor C. Piro <vitorpiro@gmail.com>
2018-12-15 18:12:01 +0100

    Fix include on ganon build test

--- tests/ganon-build/GanonBuild.test.cpp
+++ tests/ganon-build/GanonBuild.test.cpp
@@ -1,5 +1,5 @@
-//#include <ganon-build/Config.hpp>
-//#include <ganon-build/GanonBuild.hpp>
+#include <ganon-build/Config.hpp>
+#include <ganon-build/GanonBuild.hpp>
 
 #include <catch2/catch.hpp>
 
