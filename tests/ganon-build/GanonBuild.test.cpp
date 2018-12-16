b617da0992c71a55a6a877f247abc3a47895bc53
Diogo Benvenutti <diogo.a.benvenutti@gmail.com>
2018-12-16 11:05:02 +0100

    Cleanup test a bit

--- tests/ganon-build/GanonBuild.test.cpp
+++ tests/ganon-build/GanonBuild.test.cpp
@@ -5,40 +5,44 @@
 
 #include <catch2/catch.hpp>
 
+namespace detail
+{
+
+Config testConfig()
+{
+    Config cfg;
+    cfg.seqid_bin_file     = "bacteria_seqid_bin.txt";
+    cfg.output_filter_file = "test_output.filter";
+    cfg.filter_size        = 15797760;
+    cfg.kmer_size          = 19;
+    cfg.hash_functions     = 3;
+    cfg.reference_files    = { "bacteria_NC_010333.1.fasta.gz",
+                            "bacteria_NC_017163.1.fasta.gz",
+                            "bacteria_NC_017164.1.fasta.gz",
+                            "bacteria_NC_017543.1.fasta.gz" };
+    cfg.threads            = 1;
+    cfg.build_threads      = 1;
+
+    return cfg;
+}
+
+const std::string outputFile = "build_output.filter";
+
+} // namespace detail
+
 SCENARIO( "Bacteria", "[ganon-build]" )
 {
-    Config config;
-    config.seqid_bin_file     = "bacteria_seqid_bin.txt";
-    config.output_filter_file = "test_output.filter";
-    config.filter_size        = 15797760;
-    config.kmer_size          = 19;
-    config.hash_functions     = 3;
-    config.reference_files    = { "bacteria_NC_010333.1.fasta.gz",
-                               "bacteria_NC_017163.1.fasta.gz",
-                               "bacteria_NC_017164.1.fasta.gz",
-                               "bacteria_NC_017543.1.fasta.gz" };
-    config.threads            = 1;
-    config.build_threads      = 1;
+    const auto cfg = detail::testConfig();
 
-    REQUIRE( GanonBuild::run( config ) );
-    REQUIRE( aux::filesAreEqual( config.output_filter_file, "build_output.filter" ) );
+    REQUIRE( GanonBuild::run( cfg ) );
+    REQUIRE( aux::filesAreEqual( cfg.output_filter_file, detail::outputFile ) );
 }
 
 SCENARIO( "Bacteria forced failure", "[ganon-build]" )
 {
-    Config config;
-    config.seqid_bin_file     = "bacteria_seqid_bin.txt";
-    config.output_filter_file = "test_output.filter";
-    config.filter_size        = 15797760;
-    config.kmer_size          = 18;
-    config.hash_functions     = 3;
-    config.reference_files    = { "bacteria_NC_010333.1.fasta.gz",
-                               "bacteria_NC_017163.1.fasta.gz",
-                               "bacteria_NC_017164.1.fasta.gz",
-                               "bacteria_NC_017543.1.fasta.gz" };
-    config.threads            = 1;
-    config.build_threads      = 1;
+    auto cfg      = detail::testConfig();
+    cfg.kmer_size = 18;
 
-    REQUIRE( GanonBuild::run( config ) );
-    REQUIRE_FALSE( aux::filesAreEqual( config.output_filter_file, "build_output.filter" ) );
+    REQUIRE( GanonBuild::run( cfg ) );
+    REQUIRE_FALSE( aux::filesAreEqual( cfg.output_filter_file, detail::outputFile ) );
 }
