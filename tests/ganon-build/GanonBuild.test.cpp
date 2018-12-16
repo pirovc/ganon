d9b0a8986ed0ddaa9aee36b39651dcb61314ee1d
Diogo Benvenutti <diogo.a.benvenutti@gmail.com>
2018-12-16 11:01:27 +0100

    Add failure verification

--- tests/ganon-build/GanonBuild.test.cpp
+++ tests/ganon-build/GanonBuild.test.cpp
@@ -23,3 +23,22 @@ SCENARIO( "Bacteria", "[ganon-build]" )
     REQUIRE( GanonBuild::run( config ) );
     REQUIRE( aux::filesAreEqual( config.output_filter_file, "build_output.filter" ) );
 }
+
+SCENARIO( "Bacteria forced failure", "[ganon-build]" )
+{
+    Config config;
+    config.seqid_bin_file     = "bacteria_seqid_bin.txt";
+    config.output_filter_file = "test_output.filter";
+    config.filter_size        = 15797760;
+    config.kmer_size          = 18;
+    config.hash_functions     = 3;
+    config.reference_files    = { "bacteria_NC_010333.1.fasta.gz",
+                               "bacteria_NC_017163.1.fasta.gz",
+                               "bacteria_NC_017164.1.fasta.gz",
+                               "bacteria_NC_017543.1.fasta.gz" };
+    config.threads            = 1;
+    config.build_threads      = 1;
+
+    REQUIRE( GanonBuild::run( config ) );
+    REQUIRE_FALSE( aux::filesAreEqual( config.output_filter_file, "build_output.filter" ) );
+}
