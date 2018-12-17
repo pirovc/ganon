9ecff75f46b8760d120b59a88fd5787ac5b296bd
Diogo Benvenutti <diogo.a.benvenutti@gmail.com>
2018-12-17 21:46:34 +0100

    Fix return values for invalid configs

--- src/ganon-build/main.cpp
+++ src/ganon-build/main.cpp
@@ -11,5 +11,5 @@ int main( int argc, char** argv )
         return GanonBuild::run( std::move( config.value() ) ) ? EXIT_SUCCESS : EXIT_FAILURE;
     }
 
-    return EXIT_SUCCESS;
+    return EXIT_FAILURE;
 }
