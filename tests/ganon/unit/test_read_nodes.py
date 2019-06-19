774f6eff283c8ff22d95c72f32b1f7b43d6da651
Vitor C. Piro <vitorpiro@gmail.com>
2019-06-19 10:38:22 +0200

    tests python, ganon script into src

---
+++ tests/ganon/unit/test_read_nodes.py
@@ -0,0 +1,17 @@
+import unittest
+
+from src.ganon.ganon import read_nodes
+
+class TestReadNodes(unittest.TestCase):
+    def test_read_nodes(self):
+        """
+        Test if read nodes is working properly
+        """
+        nodes_file = ("tests/ganon/unit/data/mini_nodes.dmp")
+        nodes, ranks = read_nodes(nodes_file)
+        self.assertEqual(len(nodes), 24)
+        self.assertEqual(len(ranks), 24)
+        self.assertEqual(nodes["1"], "0")
+
+if __name__ == '__main__':
+    unittest.main()
