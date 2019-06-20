988eb58e9442e4fd9253fedb8e5d91d60a72bf4d
Vitor C. Piro <vitorpiro@gmail.com>
2019-06-20 15:33:12 +0200

    fix test build to check files

--- tests/ganon/integration/test_build.py
+++ tests/ganon/integration/test_build.py
@@ -1,7 +1,6 @@
 import unittest
-
-import shlex, filecmp, pickle
-
+import shlex, pickle
+from pathlib import Path
 from src.ganon import ganon
 
 class TestBuild(unittest.TestCase):
@@ -10,13 +9,16 @@ class TestBuild(unittest.TestCase):
         Test if build on sample data is working
         """
         prefix = "test_output"
-        ganon.main(shlex.split("ganon build --db-prefix " + prefix + " --taxdump-file tests/ganon/integration/data/mini_nodes.dmp tests/ganon/integration/data/mini_names.dmp --input-files tests/ganon-build/data/sequences/bacteria_NC_010333.1.fasta.gz tests/ganon-build/data/sequences/bacteria_NC_017164.1.fasta.gz tests/ganon-build/data/sequences/bacteria_NC_017163.1.fasta.gz tests/ganon-build/data/sequences/bacteria_NC_017543.1.fasta.gz"))
+        ret = ganon.main(shlex.split("ganon build --db-prefix " + prefix + " --taxdump-file tests/ganon/integration/data/mini_nodes.dmp tests/ganon/integration/data/mini_names.dmp --input-files tests/ganon-build/data/sequences/bacteria_NC_010333.1.fasta.gz tests/ganon-build/data/sequences/bacteria_NC_017164.1.fasta.gz tests/ganon-build/data/sequences/bacteria_NC_017163.1.fasta.gz tests/ganon-build/data/sequences/bacteria_NC_017543.1.fasta.gz"))
         
-        # bins can be different given python version, compare metadata (size filter, number of bins, etc)
-        for ext in ["filter", "map"]:
-            self.assertTrue(filecmp.cmp(prefix+"."+ext,"tests/ganon/integration/data/sample_bacteria."+ext, shallow=False), "File (" + ext +") is not equal")
+        # check if ran okay
+        self.assertFalse(ret, "ganon build finish with an error")
+       
+        # check if files were created
+        for ext in ["filter", "map", "bins", "nodes"]:
+            self.assertTrue(Path(prefix+"."+ext).is_file() , "File (" + ext +") was not created")
 
-        # TODO open pickle .nodes and compare variables
+        # TODO open pickle .nodes and compare variables, compare number of bins, etc...
 
 if __name__ == '__main__':
     unittest.main()
