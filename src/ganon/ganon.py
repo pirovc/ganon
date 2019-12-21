2dc148ebd9b4ca23f55147509f0b981af640bf88
Vitor C. Piro <vitorpiro@gmail.com>
2019-12-21 16:11:39 +0100

    v0.2.1

--- src/ganon/ganon.py
+++ src/ganon/ganon.py
@@ -5,7 +5,7 @@ from collections import defaultdict, OrderedDict
 
 def main(arguments=None):
 
-    version = '0.2.0'
+    version = '0.2.1'
     
     ####################################################################################################
 	
