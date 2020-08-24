379efd49467b5d092a09cdb57687040bb76a94e3
Vitor C. Piro <pirovc@posteo.net>
2020-08-24 17:16:55 +0200

    better sanity, test osx further

--- tests/ganon/integration/test_build.py
+++ tests/ganon/integration/test_build.py
@@ -27,7 +27,7 @@ class TestOffline(unittest.TestCase):
        
     def test_default(self):
         """
-        Test if build work with default paramenters
+        With default parameters
         """
         params = self.default_params.copy()
         params["db_prefix"] = self.results_dir + "TestOffline_test_default"
@@ -39,33 +39,38 @@ class TestOffline(unittest.TestCase):
         # Run
         self.assertTrue(build(cfg), "ganon build exited with an error")
         # General sanity check of results
-        self.assertTrue(sanity_check(vars(cfg)), "ganon build has inconsistent results")
+        res = sanity_check_and_parse(vars(cfg))
+        self.assertIsNotNone(res, "ganon build has inconsistent results")
 
     def test_default_main(self):
         """
-        Test if build work with default paramenters
+        With default parameters going through ganon.main()
         """
         params = self.default_params.copy()
         params["db_prefix"] = self.results_dir + "TestOffline_test_default_main"
         # Run
         self.assertTrue(ganon.main("build", **params), "ganon build exited with an error")
         # General sanity check of results
-        self.assertTrue(sanity_check(params), "ganon build has inconsistent results")
+        res = sanity_check_and_parse(params)
+        self.assertIsNotNone(res, "ganon build has inconsistent results")
 
-
-
-    def test_default_assembly(self):
+    def test_assembly(self):
         """
-        Test if build work with default paramenters
+        Default but rank=assembly
         """
         params = self.default_params.copy()
-        params["db_prefix"] = self.results_dir + "TestOffline_test_default_assembly"
+        params["db_prefix"] = self.results_dir + "TestOffline_test_assembly"
         params["rank"] = "assembly"
+        
+        # Build config from params
+        cfg = Config("build", **params)
+        # Validate
+        self.assertTrue(cfg.validate(), "Invalid configuration")
         # Run
-        self.assertTrue(ganon.main("build", **params), "ganon build exited with an error")
+        self.assertTrue(build(cfg), "ganon build exited with an error")
         # General sanity check of results
-        self.assertTrue(sanity_check(params), "ganon build has inconsistent results")
-        # 
+        res = sanity_check_and_parse(vars(cfg))
+        self.assertIsNotNone(res, "ganon build has inconsistent results")
 
 class TestOnline(unittest.TestCase):
     
@@ -80,58 +85,68 @@ class TestOnline(unittest.TestCase):
 
     def test_default(self):
         """
-        Test if build work with default paramenters without aux files
+        With default parameters
         """
         params = self.default_params.copy()
         params["db_prefix"] = self.results_dir + "TestOnline_test_default"
+        
+        # Build config from params
+        cfg = Config("build", **params)
+        # Validate
+        self.assertTrue(cfg.validate(), "Invalid configuration")
         # Run
-        self.assertTrue(ganon.main("build", **params), "ganon build exited with an error")
+        self.assertTrue(build(cfg), "ganon build exited with an error")
         # General sanity check of results
-        self.assertTrue(sanity_check(params), "ganon build has inconsistent results")
+        res = sanity_check_and_parse(vars(cfg))
+        self.assertIsNotNone(res, "ganon build has inconsistent results")
 
 def setup_build(results_dir):
     shutil.rmtree(results_dir, ignore_errors=True)
     os.makedirs(results_dir)
 
-def sanity_check(params):
+def sanity_check_and_parse(params):
+    # Provide sanity checks for outputs (not specific to a test) and return loaded data
+
     # Check if files were created
     for ext in ["ibf", "map", "tax", "gnn"]:
         f=params["db_prefix"]+"."+ext
         if not Path(f).is_file():
             print("File (" + f +") was not created")
-            return False
+            return None
         elif Path(f).stat().st_size==0:
             print("File (" + f +") is empty")
-            return False
+            return None
 
+    res = {}
     # Parse in and out files
     if "seq_info_file" in params and params["seq_info_file"]:
-        in_seq_info = parse_seq_info(params["seq_info_file"])
+        res["seq_info"] = parse_seq_info(params["seq_info_file"])
     else:
-        in_seq_info = parse_seq_info(params["db_prefix"]+".seqinfo.txt")
+        res["seq_info"] = parse_seq_info(params["db_prefix"]+".seqinfo.txt")
          
-    out_map = parse_map(params["db_prefix"]+".map")
-    out_gnn = Gnn(file=params["db_prefix"]+".gnn")
-    out_tax = parse_tax(params["db_prefix"]+".tax")
-    out_bins = Bins(taxsbp_ret=out_gnn.bins).bins
-    tax = Tax(tax_files=[params["db_prefix"]+".tax"])
+    res["gnn"] = Gnn(file=params["db_prefix"]+".gnn")
+    res["tax"] = Tax(tax_files=[params["db_prefix"]+".tax"])
+    res["bins"] = Bins(taxsbp_ret=res["gnn"].bins)
+    res["tax_pd"] = parse_tax(params["db_prefix"]+".tax")
+    res["map_pd"] = parse_map(params["db_prefix"]+".map")
+    res["bins_pd"] = res["bins"].bins
 
     # Check number of bins
-    if out_map.binid.unique().size != out_gnn.number_of_bins:
+    if res["map_pd"].binid.unique().size != res["gnn"].number_of_bins:
         print("Number of bins do not match between .gnn and .map")
-        return False
+        return None
 
     # Check if all input accession made it to the bins
-    if not in_seq_info["seqid"].isin(out_bins["seqid"]).all():
+    if not res["seq_info"]["seqid"].isin(res["bins_pd"]["seqid"]).all():
         print("Missing sequence accessions on bins")
-        return False
+        return None
 
     # Check if all taxids (chosen rank) are present in the .tax
-    if not out_tax['taxid'].apply(lambda x: tax.get_rank(x, params["rank"])).isin(out_tax["taxid"]).all():
+    if not res["tax_pd"]['taxid'].apply(lambda x: res["tax"].get_rank(x, params["rank"])).isin(res["tax_pd"]["taxid"]).all():
         print("Missing taxonomic entries")
-        return False
+        return None
 
-    return True
+    return res
 
 def parse_seq_info(seq_info_file):
     colums=['seqid', 'length', 'taxid', 'specialization']
