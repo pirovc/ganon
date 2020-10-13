f90d65e6870908978c2b66a0f49c3af0b893d0ac
Vitor C. Piro <pirovc@posteo.net>
2020-10-13 17:59:55 +0200

    more options

--- src/ganon/table.py
+++ src/ganon/table.py
@@ -18,12 +18,12 @@ def table(cfg):
     print_log(" - " + str(len(total_counts)) + " taxa found at " + cfg.rank + " level", cfg.quiet)
     
     # filter results based on top hits to all samples
-    if cfg.top_hits_all:
+    if cfg.top_all:
         top_names = []
         for i,(name,_) in enumerate(sorted(total_counts.items(), key=lambda kv: kv[1], reverse=True)):
-            if i<cfg.top_hits_all:
+            if i<cfg.top_all:
                 top_names.append(name)
-                
+
         for d in results.values():
             tre, classified_read_count, filtered_read_count = filter_names(d["data"], top_names)
             d["data"] = tre
@@ -37,7 +37,7 @@ def table(cfg):
         # reset total counts
         total_counts = get_total_counts(results)
 
-    lines = write_tsv(results, total_counts.keys(), cfg.output_file)
+    lines = write_tsv(results, total_counts.keys(), cfg)
     print_log(" - " + str(len(total_counts.keys())) + "x" + str(lines) + " table written to " + cfg.output_file, cfg.quiet)
     print_log(" - done in " + str("%.2f" % (time.time() - tx)) + "s.\n", cfg.quiet)
 
@@ -55,7 +55,7 @@ def parse_tre(file, cfg):
 
     for line in open(file):
 
-        rank, taxid, taxid_lineage, name, _, _, read_count, percentage = line.rstrip().split("\t")
+        rank, taxid, _, name, _, _, read_count, percentage = line.rstrip().split("\t")
         read_count = int(read_count)
         percentage = float(percentage)/100
         if rank == "unclassified" and not cfg.ignore_unclassified_all:
@@ -63,18 +63,18 @@ def parse_tre(file, cfg):
         elif rank == "root":
             classified_root_reads=read_count
         elif rank==cfg.rank:
-            if read_count>=cfg.min_matches and percentage>=cfg.min_perc_matches:
+            if read_count>=cfg.min_reads and percentage>=cfg.min_perc_reads:
                 classified_rank_reads += read_count
                 tre[name] = read_count
             else:
                 filtered_rank_reads += read_count
 
     # filter only top hits of each sample
-    if cfg.top_hits_sample:
+    if cfg.top_sample:
         top = dict()
         classified_rank_reads = 0 #restart to count
         for idx, (name, read_count) in enumerate(sorted(tre.items(), key=lambda kv: kv[1], reverse=True)): # sorted by read_count
-            if idx<cfg.top_hits_sample:
+            if idx<cfg.top_sample:
                 top[name] = read_count
                 classified_rank_reads += read_count
             else:
@@ -86,8 +86,8 @@ def parse_tre(file, cfg):
         tre, classified_read_count, filtered_read_count = filter_names(tre, cfg.names)
         classified_rank_reads = classified_read_count # restart to count
         filtered_rank_reads+=filtered_read_count # just add elements removed from classified
-    elif cfg.approximate_names:
-        tre, classified_read_count, filtered_read_count = filter_names(tre, cfg.approximate_names, True)
+    elif cfg.names_with:
+        tre, classified_read_count, filtered_read_count = filter_names(tre, cfg.names_with, True)
         classified_rank_reads = classified_read_count # restart to count
         filtered_rank_reads+=filtered_read_count # just add elements removed from classified
 
@@ -110,12 +110,12 @@ def parse_tre(file, cfg):
 
     return tre, total_reads, unclassified_root_reads, unclassified_rank_reads, filtered_rank_reads
 
-def filter_names(tre, names, approximate=False):
+def filter_names(tre, names, name_with=False):
     selected_names = dict()
     classified_read_count = 0
     filtered_read_count = 0
     for name, read_count in tre.items():
-        if not approximate:
+        if not name_with:
             found=name in names
         else:
             found=any(n in name for n in names)
@@ -137,16 +137,22 @@ def get_total_counts(results):
             total_counts[name] += count/d["total_reads"]
     return total_counts
 
-def write_tsv(results, names, output_file):
+def write_tsv(results, names, cfg):
     sorted_names = sorted(names)
     lines=0
-    tsv_file = open(output_file, "w")
+    tsv_file = open(cfg.output_file, "w")
     header = ["sample"] + [name for name in sorted_names]
     print(*header, sep="\t", file=tsv_file)
     for res in results.values():
         tsv_data = [res["label"]]
         for name in sorted_names:
-            tsv_data.append(res["data"][name]/res["total_reads"] if name in res["data"] else 0)
+            if name in res["data"]:
+                v = res["data"][name]
+                if cfg.output_value=="percentage":
+                    v = v/res["total_reads"]
+            else:
+                v = 0
+            tsv_data.append(v)
         if len(tsv_data) > 1 and max(tsv_data[1:]) > 0: # if there is any entry
             print(*tsv_data, sep="\t", file=tsv_file)
             lines+=1
