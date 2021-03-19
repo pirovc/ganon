5cbf9a9da6392d59b563411cb62617d98f7e09d5
Vitor C. Piro <pirovc@posteo.net>
2021-03-19 12:55:48 +0100

    remove include sdsl

--- src/ganon-build/GanonBuild.cpp
+++ src/ganon-build/GanonBuild.cpp
@@ -3,8 +3,6 @@
 #include <utils/SafeQueue.hpp>
 #include <utils/StopClock.hpp>
 
-//#include <sdsl/bit_vectors.hpp>
-
 #include <seqan3/core/debug_stream.hpp>
 #include <seqan3/io/sequence_file/input.hpp>
 #include <seqan3/range/views/kmer_hash.hpp>
