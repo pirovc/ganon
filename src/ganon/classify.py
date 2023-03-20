2c19538d2d84a713e4cc9501c68a5220d5fdcb2c
Vitor C. Piro <pirovc@posteo.net>
2023-03-20 17:25:00 +0100

    better code, fix output lca to zero

--- src/ganon/classify.py
+++ src/ganon/classify.py
@@ -27,28 +27,42 @@ def classify(cfg):
             tax_files.append(db_prefix + ".tax")
 
     # Just use if all dbs have tax
-    tax_files = ",".join(tax_files) if len(tax_files) == len(filter_files) else ""
+    tax_files = ",".join(tax_files) if len(
+        tax_files) == len(filter_files) else ""
     filter_files = ",".join(filter_files)
 
     run_ganon_classify = " ".join([cfg.path_exec['classify'],
-                                   "--single-reads " + ",".join(cfg.single_reads) if cfg.single_reads else "",
-                                   "--paired-reads " + ",".join(cfg.paired_reads) if cfg.paired_reads else "",
-                                   "--ibf " + filter_files,
-                                   "--tax " + tax_files if tax_files else "",
-                                   "--hierarchy-labels " + ",".join(cfg.hierarchy_labels) if cfg.hierarchy_labels else "",
-                                   "--rel-cutoff " + ",".join([str(rc) for rc in cfg.rel_cutoff]) if cfg.rel_cutoff else "",
-                                   "--rel-filter " + ",".join([str(rf) for rf in cfg.rel_filter]) if cfg.rel_filter else "",
-                                   "--output-prefix " + cfg.output_prefix if cfg.output_prefix else "",
-                                   "--output-lca" if cfg.output_lca else "",
-                                   "--output-all" if cfg.output_all or cfg.reassign else "",
-                                   "--output-unclassified" if cfg.output_unclassified else "",
-                                   "--output-single" if cfg.output_single else "",
-                                   "--threads " + str(cfg.threads) if cfg.threads else "",
-                                   "--n-reads " + str(cfg.n_reads) if cfg.n_reads is not None else "",
-                                   "--n-batches " + str(cfg.n_batches) if cfg.n_batches is not None else "",
-                                   "--verbose" if cfg.verbose else "",
-                                   "--hibf" if hibf else "",
-                                   "--quiet" if cfg.quiet else ""])
+                                   "--single-reads " +
+                                   ",".join(
+                                       cfg.single_reads) if cfg.single_reads else "",
+                                   "--paired-reads " +
+                                   ",".join(
+        cfg.paired_reads) if cfg.paired_reads else "",
+        "--ibf " + filter_files,
+        "--tax " + tax_files if tax_files else "",
+        "--hierarchy-labels " +
+        ",".join(
+        cfg.hierarchy_labels) if cfg.hierarchy_labels else "",
+        "--rel-cutoff " +
+        ",".join(
+        [str(rc) for rc in cfg.rel_cutoff]) if cfg.rel_cutoff else "",
+        "--rel-filter " +
+        ",".join(
+        [str(rf) for rf in cfg.rel_filter]) if cfg.rel_filter else "",
+        "--output-prefix " + cfg.output_prefix if cfg.output_prefix else "",
+        "--output-lca" if cfg.output_lca else "",
+        "--output-all" if cfg.output_all or cfg.reassign else "",
+        "--output-unclassified" if cfg.output_unclassified else "",
+        "--output-single" if cfg.output_single else "",
+        "--threads " +
+        str(cfg.threads) if cfg.threads else "",
+        "--n-reads " +
+        str(cfg.n_reads) if cfg.n_reads is not None else "",
+        "--n-batches " +
+        str(cfg.n_batches) if cfg.n_batches is not None else "",
+        "--verbose" if cfg.verbose else "",
+        "--hibf" if hibf else "",
+        "--quiet" if cfg.quiet else ""])
     stdout = run(run_ganon_classify, ret_stdout=True, quiet=cfg.quiet)
 
     if not cfg.output_prefix:
@@ -59,12 +73,11 @@ def classify(cfg):
     if cfg.reassign:
         reassign_params = {"input_prefix": cfg.output_prefix,
                            "output_prefix": cfg.output_prefix,
-                           "type": cfg.reassign,
                            "verbose": cfg.verbose,
                            "quiet": cfg.quiet}
         reassign_cfg = Config("reassign", **reassign_params)
         ret = reassign(reassign_cfg)
-        if ret==False:
+        if ret == False:
             return ret
 
     if cfg.output_prefix and tax_files:
--- src/ganon/reassign.py
+++ src/ganon/reassign.py
@@ -20,7 +20,7 @@ def reassign(cfg):
         # look for hiearchies
         with open(rep_file) as rep:
             for line in rep:
-                if line[0]!="#":
+                if line[0] != "#":
                     all_files[line.split("\t")[0]] = ""
                 else:
                     rep_file_info.append([line.rstrip()])
@@ -35,7 +35,8 @@ def reassign(cfg):
                 all_files[""] = cfg.input_prefix + ".all"
                 break
             else:
-                print_log("No matching files for given report [" + cfg.input_prefix + ".all]", cfg.quiet)
+                print_log(
+                    "No matching files for given report [" + cfg.input_prefix + ".all]", cfg.quiet)
                 return False
     else:
         print_log("No report file found " + rep_file, cfg.quiet)
@@ -45,23 +46,24 @@ def reassign(cfg):
             all_files[""] = cfg.input_prefix + ".all"
 
     if not all_files:
-        print_log("No .rep or .all file(s) found with prefix --input-prefix " + cfg.input_prefix, cfg.quiet)
+        print_log("No .rep or .all file(s) found with prefix --input-prefix " +
+                  cfg.input_prefix, cfg.quiet)
         return False
 
     print_log("Reassigning reads", cfg.quiet)
     new_rep = []
-    init_var = 1 if cfg.type=="ones" else 0
     for hierarchy, af in all_files.items():
 
-        print_log(af + (" [" + hierarchy + "]" if hierarchy else ""), cfg.quiet)
+        print_log(
+            af + (" [" + hierarchy + "]" if hierarchy else ""), cfg.quiet)
 
-        # transoform target string into int
+        # Auto-increment dict to save targets (string) into integers, less memory
         targets = defaultdict(lambda: len(targets))
 
+        prob = {}
         read_matches = {}
-        unique_matches = {}
-        
-        total_umatches = 0
+        initial_weight = {}
+
         with open(af, "r") as all_file:
             for line in all_file:
                 readid, target, kcount = line.rstrip().split("\t")
@@ -69,64 +71,46 @@ def reassign(cfg):
                     read_matches[readid] = []
                 read_matches[readid].append((targets[target], int(kcount)))
 
-                # Not all targets have unique matches, initialize
-                if targets[target] not in unique_matches:
-                    unique_matches[targets[target]] = init_var
-                    total_umatches+=1
-        
-        
-        if cfg.type=="unique":
-            total_umatches = 0
-            for matches in read_matches.values():
-                if len(matches) == 1:
-                    total_umatches += 1
-                    unique_matches[matches[0][0]] += 1
-        elif cfg.type=="matches":
-            total_umatches = 0
-            for matches in read_matches.values():
-                for m, _ in matches:
-                    total_umatches += 1
-                    unique_matches[m] += 1
+                # Not all targets have unique matches, initialize all targets with zero weights
+                if targets[target] not in initial_weight:
+                    initial_weight[targets[target]] = 0
 
-        # Calculate first probabilities based on unique matche
-        prob = {}
-        for target, unique in unique_matches.items():
-            prob[target] = unique / total_umatches
+        # Assign unique matches as initial weights
+        # Calculate first probabilities based on weights (unique matches)
+        total_weight = len(read_matches)
+        total_initial_weight = 0
+        for matches in read_matches.values():
+            if len(matches) == 1:
+                total_initial_weight += 1
+                initial_weight[matches[0][0]] += 1
+
+        for target, unique in initial_weight.items():
+            prob[target] = unique / total_initial_weight
 
         # EM loop
         for i in range(cfg.max_iter):
 
-            total_reassigned = 0
-            reassigned_matches = unique_matches.copy()
+            # Make copy of initial weights
+            reassigned_matches = initial_weight.copy()
 
             for matches in read_matches.values():
-                if len(matches) == 1:
-                    continue
-
-                # Set first match as target, in case all targets have no unique matches
-                # loop will also randonly get one (last) if prob is equal
-                max_target = matches[0][0]
-                max_p = 0
-                # print(matches)
-
-                for m, _ in matches:
-                    if prob[m] > max_p:
-                        max_p = prob[m]
-                        max_target = m
-
-                reassigned_matches[max_target] += 1
-                total_reassigned += 1
+                # Skip unique matches
+                if len(matches) > 1:
+                    # Get match with highest probability in this round
+                    t, _ = get_top_match(matches, prob)
+                    reassigned_matches[t] += 1
 
             diff = 0
-            # Calculate new probabilities based on re-distribution
+            # Calculate new probabilities based on "simulated" re-distributed reads for the next round
             for target, count in reassigned_matches.items():
-                new_prob = count / (total_umatches+total_reassigned)
+                new_prob = count / total_weight
                 diff += abs(prob[target] - new_prob)
                 prob[target] = new_prob
 
-            print_log(" - Iteration " + str(i+1) + " (" + str(round(diff,6)) + ")", cfg.quiet)
+            print_log(" - Iteration " + str(i+1) +
+                      " (" + str(round(diff, 6)) + ")", cfg.quiet)
 
-            # Converged
+            # If abs. difference among old and new probabilities already converged (no change)
             if diff <= cfg.threshold:
                 break
 
@@ -137,7 +121,7 @@ def reassign(cfg):
             file_pre = os.path.splitext(os.path.basename(af))[0]
             output_file = cfg.output_prefix + "." + hierarchy + ".all"
 
-        #reverse target <-> id
+        # reverse string target <-> integer id
         targets_rev = {val: key for (key, val) in targets.items()}
         reassigned_reads = 0
         with open(output_file, "w") as out_file:
@@ -147,15 +131,9 @@ def reassign(cfg):
                           [1], sep="\t", file=out_file)
                 else:
                     reassigned_reads += 1
-                    max_target = matches[0][0]
-                    max_p = 0
-                    kcount = 0
-                    for m, k in matches:
-                        if prob[m] > max_p:
-                            max_p = prob[m]
-                            max_target = m
-                            kcount = k
-                    print(readid, targets_rev[max_target], k, sep="\t", file=out_file)
+                    target, kcount = get_top_match(matches, prob)
+                    print(readid, targets_rev[target],
+                          kcount, sep="\t", file=out_file)
 
         print_log(" - " + str(reassigned_reads) +
                   " reassigned reads: " + output_file, cfg.quiet)
@@ -165,10 +143,13 @@ def reassign(cfg):
         if rep_file_out:
             with open(rep_file, "r") as rep:
                 for line in rep:
-                    if line[0]!="#":
-                        hierarchy_name, target, direct_matches, unique_reads, lca_reads, rank, name = line.rstrip().split("\t")
-                        if (hierarchy=="" or hierarchy_name==hierarchy) and targets[target] in reassigned_matches:
-                            new_rep.append([hierarchy_name, target, direct_matches, reassigned_matches[targets[target]], lca_reads, rank, name])
+                    if line[0] != "#":
+                        hierarchy_name, target, direct_matches, _, _, rank, name = line.rstrip().split("\t")
+                        # Only print line of targets
+                        if (hierarchy == "" or hierarchy_name == hierarchy) and targets[target] in reassigned_matches:
+                            # LCA matches are zero, since they will be reassigned to other nodes
+                            new_rep.append([hierarchy_name, target, direct_matches,
+                                            reassigned_matches[targets[target]], 0, rank, name])
 
     if rep_file_out:
         with open(rep_file_out, "w") as rep_out:
@@ -178,5 +159,20 @@ def reassign(cfg):
                 print(*info, sep="\t", file=rep_out)
         print_log("New report file: " + rep_file_out, cfg.quiet)
 
-
     return True
+
+
+def get_top_match(matches, prob):
+
+    # Set first match as target, in case all targets have no unique matches
+    # loop will also randonly get one (last) if prob is equal
+    target = matches[0][0]
+    kcount = matches[0][1]
+    max_p = 0
+    for m, k in matches:
+        if prob[m] > max_p:
+            max_p = prob[m]
+            target = m
+            kcount = k
+
+    return target, kcount
