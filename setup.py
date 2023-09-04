1b8783522346d6a68806968ddeeff835301f485d
Vitor C. Piro <pirovc@posteo.net>
2023-09-04 16:13:28 +0200

    v1.9.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(filename):
 
 setup(
     name="ganon",
-    version="1.8.0",
+    version="1.9.0",
     url="https://www.github.com/pirovc/ganon",
     license='MIT',
     author="Vitor C. Piro",
