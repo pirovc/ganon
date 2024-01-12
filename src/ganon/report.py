1d9fc12bf27a7ec800d94e08a6af334ff8472618
Vitor C. Piro <pirovc@posteo.net>
2024-01-12 19:39:01 +0100

    use dummy tax. with skip

--- src/ganon/report.py
+++ src/ganon/report.py
@@ -7,7 +7,7 @@ from ganon.util import validate_input_files
 from ganon.util import print_log
 from ganon.tax_util import get_genome_size, parse_genome_size_tax
 
-from multitax import CustomTx, NcbiTx, GtdbTx
+from multitax import CustomTx, NcbiTx, GtdbTx, DummyTx
 
 
 def report(cfg):
@@ -50,16 +50,19 @@ def report(cfg):
         if cfg.taxonomy_files:
             print_log("Parsing " + cfg.taxonomy + " taxonomy", cfg.quiet)
         else:
-            print_log("Downloading and parsing " +
-                      cfg.taxonomy + " taxonomy", cfg.quiet)
+            if cfg.taxonomy == "skip":
+                tax = DummyTx(**tax_args)
+            else:
+                print_log("Downloading and parsing " +
+                          cfg.taxonomy + " taxonomy", cfg.quiet)
 
-        if cfg.taxonomy == "ncbi":
-            tax = NcbiTx(files=cfg.taxonomy_files, **tax_args)
-        elif cfg.taxonomy == "gtdb":
-            tax = GtdbTx(files=cfg.taxonomy_files, **tax_args)
+                if cfg.taxonomy == "ncbi":
+                    tax = NcbiTx(files=cfg.taxonomy_files, **tax_args)
+                elif cfg.taxonomy == "gtdb":
+                    tax = GtdbTx(files=cfg.taxonomy_files, **tax_args)
 
-        print_log(" - done in " + str("%.2f" %
-                                      (time.time() - tx)) + "s.\n", cfg.quiet)
+                print_log(" - done in " + str("%.2f" %
+                                              (time.time() - tx)) + "s.\n", cfg.quiet)
 
         # In case no tax was provided, generate genome sizes (for the full tree)
         if cfg.report_type in ["abundance", "corr"]:
