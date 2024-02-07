e04ff1e70dc2a7f3b68cdfc602de32be2e5c0d57
Vitor C. Piro <pirovc@posteo.net>
2024-02-07 10:34:56 +0100

    ganon v2.1.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(filename):
 
 setup(
     name="ganon",
-    version="2.0.1",
+    version="2.1.0",
     url="https://www.github.com/pirovc/ganon",
     license='MIT',
     author="Vitor C. Piro",
