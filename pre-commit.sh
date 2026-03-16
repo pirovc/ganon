14aa30f422ced5916152ab12d2dd7a4461bdba8e
pirovc <pirovc@posteo.net>
2026-03-16 11:02:08 +0100

    automate params docs

--- pre-commit.sh
+++ pre-commit.sh
@@ -5,3 +5,6 @@ ruff check --fix
 # C++
 find src/ -iname '*.hpp' -o -iname '*.cpp' | xargs clang-format -i
 find tests/ -iname '*.hpp' -o -iname '*.cpp' | xargs clang-format -i
+
+# Update docs
+docs/generate_params.sh > docs/params.md
